08be26e12ff16f86b2998aafde02a564b44cce24
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-01-05 14:52:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -361,30 +361,3 @@ pie_data.T.plot.pie(subplots=True, autopct='%1.1f%%', legend=False, startangle=9
 plt.title(f"Recommendations for {symbol} on {exchange} - {interval}")
 plt.legend()
 st.pyplot(plt)
-
-st.markdown("""
-<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js" async>
-  {
-  "exchanges": [],
-  "dataSource": "SENSEX",
-  "grouping": "sector",
-  "blockSize": "market_cap_basic",
-  "blockColor": "change",
-  "locale": "en",
-  "symbolUrl": "",
-  "colorTheme": "light",
-  "hasTopBar": false,
-  "isDataSetEnabled": false,
-  "isZoomEnabled": true,
-  "hasSymbolTooltip": true,
-  "width": "100%",
-  "height": "100%"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
-""" ,unsafe_allow_html=True )
